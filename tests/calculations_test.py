0fb7e5667de0f9c0daf9ec21f12f8fb58401d2eb
carlosjimpizango <cpizangos@autonoma.edu.pe>
2025-09-14 00:06:08 -0500

    Update calculations_test.py

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -9,7 +9,6 @@ import pytest
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 from calculations import area_of_circle, get_nth_fibonacci   # noqa: E402
 
-
 def test_area_of_circle_positive_radius():
     """Test with a positive radius."""
     # Arrange
